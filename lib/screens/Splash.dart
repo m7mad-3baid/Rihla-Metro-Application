ca9a4d8c7eb7a96061d938b7dcb66fbefbc54d96
Mohammed Obaid <98846807+m7mad-3baid@users.noreply.github.com>
2026-07-17 20:07:59 +0300

    chenged the push action to loginpage
    chenged the push action to loginpage  after the animation ends to the login page instead of main screen

--- lib/screens/Splash.dart
+++ lib/screens/Splash.dart
@@ -1,96 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:rihla_4_0/screens/MainScreen.dart';
+import 'package:rihla_4_0/screens/loginpage.dart';
 import 'package:video_player/video_player.dart';
 import 'routes_screen.dart';
 
 class Splash extends StatefulWidget {
   const Splash({super.key});
-  
 
   @override
   State<Splash> createState() => _SplashState();
 }
 
-
 class _SplashState extends State<Splash> {
-
   late VideoPlayerController _controller;
-
 
   @override
   void initState() {
     super.initState();
 
-    _controller = VideoPlayerController.asset(
-      'assets/Video/Splash.mp4',
-    );
-
+    _controller = VideoPlayerController.asset('assets/Video/Splash.mp4');
 
     _controller.initialize().then((value) {
-
       setState(() {});
 
       _controller.play();
 
-
       _controller.addListener(() {
-
-        if (_controller.value.position >=
-            _controller.value.duration) {
-
-
+        if (_controller.value.position >= _controller.value.duration) {
           Navigator.pushReplacement(
             context,
-            MaterialPageRoute(
-              builder: (context) => const MainScreen(),
-            ),
+            MaterialPageRoute(builder: (context) => const Loginpage()),
           );
-
-
         }
-
       });
-
-
     });
-
   }
-
-
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-
       backgroundColor: const Color.fromARGB(255, 232, 231, 233),
 
       body: Center(
-
         child: _controller.value.isInitialized
-
             ? AspectRatio(
                 aspectRatio: _controller.value.aspectRatio,
                 child: VideoPlayer(_controller),
               )
-
             : const CircularProgressIndicator(),
-
       ),
-
     );
-
   }
-
-
 
   @override
   void dispose() {
-
     _controller.dispose();
 
     super.dispose();
-
   }
-
 }
